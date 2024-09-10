5f45e16847cb4a55ca15704605c5b936644dd11d
Brian Waldschmidt <159931728+bwaldschmidt103@users.noreply.github.com>
2024-09-10 15:33:26 -0500

    Add files via upload

---
+++ object-assessment-sample/scripts/myScript.js
@@ -0,0 +1,144 @@
+const food1 = {
+    name: "Baked Goat Cheese Dip",
+    category: "Appetizer",
+    image: "images/baked-goat-cheese-dip.jpg",
+    ingredients: "1 10-ounce log goat cheese (at room temperature), " +
+        "4 ounces cream cheese (at room temperature), 1/4 cup grated parmesan cheese, " +
+        "1/4 cup extra - virgin olive oil (plus more or brushing), Freshly ground pepper, " +
+        "8 small cocktail tomatoes (quartered(about 10 ounces)), " +
+        "2 tablespoons chopped fresh chives, 2 teaspoons balsamic vinegar," +
+        "and 1 clove garlic (finely chopped), Kosher salt," +
+        "Toasted baguette slices (for serving).",
+    directions: '(1) Preheat the oven to 400 degrees F. Combine the goat cheese, ' +
+        ' cream cheese,  parmesan and 2 tablespoons olive oil in a food processor and ' + 'season with pepper; puree until smooth. Brush a 1-quart baking dish with olive' + 'oil, then spread the cheese mixture in the dish, mounding it slightly higher' + 'around the edge than in the middle. Bake until golden and heated through, about' + '15 minutes.' +
+        '<br>(2) Meanwhile, combine the tomatoes, chives, vinegar, garlic, the remaining 2' + 'tablespoons olive oil and 1 / 2 teaspoon salt in a medium bowl.Spoon the tomato' + 'mixture on top of the dip.Serve with baguette slices.',
+    source: "https://www.foodnetwork.com/recipes/food-network-kitchen/baked-goat-cheese-dip-3363801"
+};
+
+const food2 = {
+    name: "Barbeque Mushroom Pizza",
+    category: "Lunch/Dinner",
+    image: "images/barbecue-mushroom-pizza.jpg",
+    ingredients: "1/4 cup apple cider vinegar, 1 teaspoon sugar, Kosher salt, " +
+        "1/4 small red onion (thinly sliced), 6 tablespoons extra-virgin olive oil, " +
+        "3/4 pound oyster mushrooms (torn into thin strips), Freshly ground pepper, " +
+        "3 cloves garlic (finely chopped), 1/3 cup plus 1/4 cup barbecue sauce, " +
+        "1 15- to 16-ounce ball pizza dough, 4 ounces smoked gouda cheese (shredded (about 1 1/3 cups)), " +
+        "2 ounces Monterey Jack cheese (shredded (about 1/2 cup)), 1 scallion (thinly sliced), " +
+        " and 2 tablespoons chopped fresh cilantro.",
+    directions: '(1) Place a pizza stone or inverted baking sheet on the middle oven rack and preheat to 450˚ F.' +
+        ' Whisk the vinegar, sugar and a pinch of salt in a small bowl, then add the sliced red onion and submerge in the vinegar; set aside to pickle.' +
+        '<br>(2) Heat 3 tablespoons olive oil in a large nonstick skillet over medium-high heat. Add the mushrooms, season with salt and pepper and cook, undisturbed, until golden brown on the bottom, 5 minutes. Stir and continue to cook, stirring occasionally, until crisp in spots and golden brown, 5 more minutes. Add the garlic and cook, stirring, 1 minute. Add 1/3 cup barbecue sauce and 2 tablespoons water and cook, stirring, until most of the liquid is absorbed and the mushrooms are glazed, 2 to 3 minutes. Remove from the heat.' +
+        '<br>(3) Coat a baking sheet with the remaining 3 tablespoons olive oil. Add the pizza dough and stretch into an 11-by-13-inch oval. Top with the remaining 1/4 cup barbecue sauce and spread using the back of a spoon, leaving a 1/2-inch border. Sprinkle with the gouda, then top with the mushrooms, then the Monterey Jack. Put the baking sheet directly on the hot pizza stone or inverted baking sheet. Bake until the crust is golden brown on the bottom and around the edges, 15 to 20 minutes.' +
+        '<br>(4) Slide the pizza onto a large cutting board. Drain the red onion and scatter over the pizza, along with the scallion and cilantro. Cut into pieces.',
+    source: "https://www.foodnetwork.com/recipes/barbecue-mushroom-pizza-12134318"
+};
+
+const food3 = {
+    name: "Caramel Apple Soft Pretzels",
+    category: "Appetizer",
+    image: "images/caramel-apple-soft-pretzels.jpg",
+    ingredients: "Pretzels: 1 cup whole milk (warmed to 110 degrees F), " +
+        "One 1 / 4 - ounce package active dry yeast(2 1/4 teaspoons), " +
+        "3 tablespoons packed light brown sugar, 1 teaspoon pure vanilla extract, " +
+        "2 1/4 cups all-purpose flour (plus more for kneading (see Cook 's Note)), " +
+        "2 tablespoons unsalted butter (at room temperature (plus more for greasing the bowl)), 1 small Granny Smith apple (peeled and finely chopped into 1/4 - inch pieces), 1 teaspoon ground cinnamon, 1 teaspoon kosher salt, 1/3 cup baking soda, " +
+        "2 tablespoons coarse salt." +
+        "<br>Caramel Glaze: 2 tablespoons unsalted butter, " +
+        "1/3 cup packed light brown sugar, 1/4 cup heavy cream, 1 cup confectioners', " + "sugar (sifted), 1/2 teaspoon pure vanilla extract, and a pinch of kosher salt. ",
+    directions: '(1) For the pretzels: Pour the warm milk into a medium bowl and sprinkle the yeast over. Let the yeast bloom until softened and foamy, about 5 minutes.' +
+        '<br>(2) Stir in the brown sugar, vanilla and 1 cup of the flour with a wooden spoon until just combined. Stir in the softened butter until incorporated, then add the remaining 1 1/4 cups flour, chopped apple, cinnamon and kosher salt to make a sticky dough.' +
+        '<br>(3) Turn the dough out onto a lightly floured surface and knead, adding more flour if needed, until smooth but still slightly tacky, about 5 minutes. Shape into a ball, place in a lightly buttered large bowl and cover tightly with plastic wrap. Let rise in a warm place until doubled in size, about 1 hour.' +
+        '<br>(4) Meanwhile, preheat the oven to 450 degrees F and line a rimmed baking sheet with parchment paper.' +
+        '<br>(5) Punch the dough down to deflate it, then turn out onto a lightly floured surface. (If the dough seems tight, cover it and let rest until it relaxes.) Divide the dough into 6 pieces.' +
+        '<br>(6) Working with one piece at a time, roll and stretch the dough with the palms of your hands into a 24-inch-long rope. Lift the ends of the rope to make a U shape, then cross one end over the other twice to make a double twist in the middle. Fold down the ends and attach them to the bottom of the U, creating a pretzel shape. Repeat with the remaining dough.' +
+        '<br>(7) Dissolve the baking soda in 3 cups hot water in a shallow baking dish. Gently dip each pretzel in the soda solution, let some of the excess water drip off, then arrange on the prepared baking sheet and sprinkle with the coarse salt. Bake until the pretzels are puffed and deep golden brown, 12 to 15 minutes. Set aside to cool.' +
+        '<br>(8) For the caramel glaze: Melt the butter and brown sugar in a small saucepan over medium-low heat until completely melted and smooth. Whisk in the heavy cream until combined, then transfer to a medium bowl. Whisk in the confectioners\' sugar, vanilla and salt until smooth (it should be thick enough to drizzle over the pretzels but not so thin that it drips away). Immediately transfer to a small piping bag or resealable bag, snip the corner and drizzle over the cooled pretzels. Let the glaze set, about 10 minutes.',
+    source: "https://www.foodnetwork.com/recipes/food-network-kitchen/caramel-apple-soft-pretzels-8840886"
+};
+
+const food4 = {
+    name: "Salmon Cakes",
+    category: "Dinner",
+    image: "images/salmon-cakes.jpg",
+    ingredients: "1/2 pound fresh salmon, good olive oil, kosher salt and freshly ground black pepper, 4 tablespoons unsalted butter, 3/4 cup small-diced red onion (1 small onion), " +
+        "1 1/2 cups small-diced celery (4 stalks), 1/2 cup small-diced red bell pepper (1 small pepper), 1/2 cup small-diced yellow bell pepper (1 small pepper), " +
+        "1/4 cup minced fresh flat-leaf parsley, 1 tablespoon capers (drained), 1/4 teaspoon hot sauce (recommended: Tabasco), 1/2 teaspoon Worcestershire sauce, " +
+        "1 1/2 teaspoons crab boil seasoning (recommended: Old Bay), 3 slices stale bread (crusts removed), 1/2 cup good mayonnaise, 2 teaspoons Dijon mustard, " +
+        "2 extra-large eggs (lightly beaten). ",
+    directions: '(1) Preheat the oven to 350 degrees F.' +
+        '<br>(2) Place the salmon on a sheet pan, skin side down. Brush with olive oil and sprinkle with salt and pepper. Roast for 15 to 20 minutes, until just cooked. Remove from the oven and cover tightly with aluminum foil. Allow to rest for 10 minutes and refrigerate until cold.' +
+        '<br>(3) Meanwhile, place 2 tablespoons of the butter, 2 tablespoons olive oil, the onion, celery, red and yellow bell peppers, parsley, capers, hot sauce, Worcestershire sauce, crab boil seasoning, 1/2 teaspoon salt, and 1/2 teaspoon pepper in a large saute pan over medium-low heat and cook until the vegetables are soft, approximately 15 to 20 minutes. Cool to room temperature.' +
+        '<br>(4) Break the bread slices in pieces and process the bread in a food processor fitted with a steel blade. You should have about 1 cup of bread crumbs. Place the bread crumbs on a sheet pan and toast in the oven for 5 minutes until lightly browned, tossing occasionally.' +
+        '<br>(5) Flake the chilled salmon into a large bowl. Add the bread crumbs, mayonnaise, mustard, and eggs. Add the vegetable mixture and mix well. Cover and chill in the refrigerator for 30 minutes. Shape into 10 (2 1/2 to 3-ounce) cakes.' +
+        '<br>(6) Heat the remaining 2 tablespoons butter and 2 tablespoons olive oil in a large saute pan over medium heat. In batches, add the salmon cakes and fry for 3 to 4 minutes on each side, until browned. Drain on paper towels; keep them warm in a preheated 250 degree F oven and serve hot.',
+    source: "https://www.foodnetwork.com/recipes/ina-garten/salmon-cakes-recipe-1916217"
+};
+
+const food5 = {
+    name: "Sheet Pan London Broil with Asparagus Gribiche",
+    category: "Dinner",
+    image: "images/sheet-pan-london-broil-with-asparagus-gribiche.jpg",
+    ingredients: "London Broil: 2 tablespoons extra-virgin olive oil," +
+        "2 tablespoons Worcestershire sauce, 4 cloves garlic (smashed), Juice of 1 lemon, " +
+        "Kosher salt and freshly ground black pepper, 2 pounds top round London broil steak (1/2 inch to 1 inch thick.)" +
+        "<br>Gribiche: 3 large eggs, 1/4 cup extra-virgin olive oil, 1 tablespoon Dijon mustard, " +
+        "1 tablespoon white wine vinegar, 1 clove garlic (grated), " +
+        "Kosher salt and freshly ground black pepper, 1 cup fresh parsley (finely chopped), " +
+        "2 tablespoons dried chives, 1 tablespoon drained capers (roughly chopped), " +
+        "6 cornichons (roughly chopped). " +
+        "<br>Asparagus: 2 pounds pencil-thin asparagus, woody stems (removed), " +
+        "1 tablespoon extra-virgin olive oil, Kosher salt and freshly ground black pepper. ",
+    directions: '(1) For the London broil: Set a large resealable plastic bag in a' +
+        'medium saucepan and pull the edges of the bag over the sides of the pan for easy access to the bag. Add the olive oil, Worcestershire sauce, garlic cloves, lemon juice, 1 teaspoon salt and 1 teaspoon pepper to the bag and whisk to combine. Add the London broil and seal the bag tightly. Remove the bag from the pan and use your hands to rotate the meat, coating thoroughly in the marinade. Lay the bag flat on a cutting board and marinate at room temperature for 1 hour, flipping the bag after 30 minutes. (The seasoned meat can also be stored in the refrigerator overnight. Let rest at room temperature for 20 minutes before cooking.)' +
+        '<br>(2) For the gribiche: Meanwhile, bring a medium saucepan filled with water to a boil over high heat. Use a large spoon to gently add the eggs. Boil for 10 minutes. Drain the eggs and rinse under cold water to stop the cooking. Crack, peel and roughly chop the eggs.' + '<br>(3) Whisk the oil, mustard, vinegar, garlic and 1/2 teaspoon each of salt and pepper in a medium bowl. Stir in the parsley, chives, capers and cornichons. Fold in the chopped eggs. Cover and set aside to marinate. (The gribiche can be refrigerated for up to 2 days; return to room temperature before using.)' +
+        '<br>(4) While the gribiche is marinating, position an oven rack about 4 inches from the heating element and preheat the broiler to high. Cover a rimmed baking sheet with aluminum foil and place in the oven to preheat.' +
+        '<br>(5) Remove the steak from the bag and discard any stuck-on garlic cloves. Pull the preheated sheet pan from the oven and place the steak in the center. Broil until nicely charred and an instant-read thermometer inserted sideways into the center of the steak registers between 120 and 125 degrees F, 6 to 10 minutes depending on thickness. Transfer the steak to the cutting board and let rest for 10 minutes.' +
+        '<br>(6) For the asparagus: While the steak rests, add the asparagus to the same sheet pan and toss with the olive oil. Season with 1/2 teaspoon salt and 1/4 teaspoon pepper. Broil until the asparagus is vibrantly green and crisp-tender, about 3 minutes.' +
+        '<br>(7) Divide the asparagus among plates and spoon the gribiche over the top. Thinly slice the rested steak against the grain and divide among the plates.',
+    source: "https://www.foodnetwork.com/recipes/sheet-pan-london-broil-with-asparagus-gribiche-12159663"
+};
+
+function display() {
+    let html1 = `<h2>${food1.name}</h2>` +
+        `<h3>${food1.category}</h3> <img src = ${food1.image}>` +
+        `<h3>Ingredients</h3>` +
+        `<p>${food1.ingredients}<p>` + `<h3>Directions</h3>` +
+        `<p>${food1.directions}<p>` + `<h3>Source</h3>` +
+        `<p><a href="${food1.source}">${food1.source}</a></p>`;
+
+    let html2 = `<h2>${food2.name}</h2>` +
+        `<h3>${food2.category}</h3> <img src = ${food2.image}>` +
+        `<h3>Ingredients</h3>` +
+        `<p>${food2.ingredients}<p>` + `<h3>Directions</h3>` +
+        `<p>${food2.directions}<p>` + `<h3>Source</h3>` +
+        `<p><a href="${food2.source}">${food2.source}</a></p>`;
+
+    let html3 = `<h2>${food3.name}</h2>` +
+        `<h3>${food3.category}</h3> <img src = ${food3.image}>` +
+        `<h3>Ingredients</h3>` +
+        `<p>${food3.ingredients}<p>` + `<h3>Directions</h3>` +
+        `<p>${food3.directions}<p>` + `<h3>Source</h3>` +
+        `<p><a href="${food3.source}">${food3.source}</a></p>`;
+
+    let html4 = `<h2>${food4.name}</h2>` +
+        `<h3>${food4.category}</h3> <img src = ${food4.image}>` +
+        `<h3>Ingredients</h3>` +
+        `<p>${food4.ingredients}<p>` + `<h3>Directions</h3>` +
+        `<p>${food4.directions}<p>` + `<h3>Source</h3>` +
+        `<p><a href="${food4.source}">${food4.source}</a></p>`;
+
+    let html5 = `<h2>${food5.name}</h2>` +
+        `<h3>${food5.category}</h3> <img src = ${food5.image}>` +
+        `<h3>Ingredients</h3>` +
+        `<p>${food5.ingredients}<p>` + `<h3>Directions</h3>` +
+        `<p>${food5.directions}<p>` + `<h3>Source</h3>` +
+        `<p><a href="${food5.source}">${food5.source}</a></p>`;
+
+    document.getElementById('ob1').innerHTML = html1;
+    document.getElementById('ob2').innerHTML = html2;
+    document.getElementById('ob3').innerHTML = html3;
+    document.getElementById('ob4').innerHTML = html4;
+    document.getElementById('ob5').innerHTML = html5;
+
+};
