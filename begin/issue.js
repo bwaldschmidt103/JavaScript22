20e2766649f353ac54f4f96421accf082e091b2f
Brian Waldschmidt <159931728+bwaldschmidt103@users.noreply.github.com>
2024-10-29 22:16:56 -0500

    Add files via upload

---
+++ begin/issue.js
@@ -0,0 +1,11 @@
+const queue = '[{"id":"394","title":"Eat your veggies"},{"id":"378","title":"Every step counts"},{"id":"406","title":"Giving Back"}]';
+
+// Enter code to destringify the `queue` variable here:
+let destringifiedQueue = JSON.parse(queue);
+console.log(destringifiedQueue);
+
+const item = '{"id":"406","title":"Giving Back"}';
+
+// Enter code to destringify the `item` variable here:
+let destringifiedItem = JSON.parse(item);
+console.log(destringifiedItem);
