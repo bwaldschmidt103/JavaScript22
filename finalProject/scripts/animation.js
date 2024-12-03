c41dc3d1126c2cff3cf0917ec7966f6cb09c9cd8
Brian Waldschmidt <159931728+bwaldschmidt103@users.noreply.github.com>
2024-12-03 14:33:47 -0600

    Add files via upload

---
+++ finalProject/scripts/animation.js
@@ -0,0 +1,7 @@
+// animation.js
+$(document).ready(function() {
+    // Animates the div to the center of the viewport
+    $('#animated-div').animate({
+        left: '45%'   // Horizontal positioning control
+    }, 'slow');
+});
