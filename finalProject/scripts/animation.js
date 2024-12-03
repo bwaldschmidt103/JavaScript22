3f90e0e9853cd17311866a2ce2409d6577cea82a
Brian Waldschmidt <159931728+bwaldschmidt103@users.noreply.github.com>
2024-12-03 15:25:52 -0600

    Delete finalProject directory

--- finalProject/scripts/animation.js
+++
@@ -1,7 +0,0 @@
-// animation.js
-$(document).ready(function() {
-    // Animates the div to the center of the viewport
-    $('#animated-div').animate({
-        left: '45%'   // Horizontal positioning control
-    }, 'slow');
-});
